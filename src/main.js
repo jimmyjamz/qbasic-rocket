c50c2a5b933d6ca6f5543bb4b2f0bd75eb427fab
jimmyjamz <sjamro@gmail.com>
2026-08-30 13:49:17 -0400

    Wire rescue NPC placeholder presentation

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './planetExplorer.js';
 import './countdownPresentation.js';
 import './launchCrewPresentation.js';
+import './rescueNpcPresentation.js';
 import './missionTrackerPresentation.js';
