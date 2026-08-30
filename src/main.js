52094c991e3ba415a24df3a1451b6c08f01cb135
jimmyjamz <sjamro@gmail.com>
2026-08-30 13:58:24 -0400

    Wire rescue completion reward presentation

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import './planetExplorer.js';
 import './countdownPresentation.js';
 import './launchCrewPresentation.js';
 import './rescueNpcPresentation.js';
+import './rescueCompletionPresentation.js';
 import './missionTrackerPresentation.js';
