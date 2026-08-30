c10b2ddc07bb5dbd9392957a5c149b003382d499
jimmyjamz <sjamro@gmail.com>
2026-08-30 14:37:13 -0400

    Wire rescue passenger and session rewards presentations

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import './countdownPresentation.js';
 import './launchCrewPresentation.js';
 import './rescueNpcPresentation.js';
 import './rescueCompletionPresentation.js';
+import './rescuePassengerPresentation.js';
+import './sessionRewardsPresentation.js';
 import './missionTrackerPresentation.js';
